385e3615b400088a25028fa999d3cbbc501d545c
Jakub Ząski <zbaskij@gmail.com>
2019-07-03 22:07:53 +0200

    Added mobile responsiveness

--- src/components/Navigation/components/SidebarMenu/index.js
+++ src/components/Navigation/components/SidebarMenu/index.js
@@ -1,24 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid } from "@material-ui/core";
+import { Grid, Hidden } from "@material-ui/core";
 import Close from "@material-ui/icons/Close";
 import logo from "./images/logo.png";
 import { Link } from "react-router-dom";
+import SearchComponent from "../SearchComponent";
+import ShareIcon from "../../../ShareIcon";
 
 import styles from "./styles.module.css";
 
 const SidebarMenu = props => (
   <div className={styles.sidebarMenuCotainer}>
     <Grid container className={styles.menuHead}>
-      <Grid item xs={1} />
-      <Grid item xs={3} className={styles.closeBtn}>
-        <Close onClick={props.onMenuClosed} />
+      <Grid item xs={false} md={1} />
+      <Grid item xs={2} md={3} className={styles.closeBtn}>
+        <Close onClick={props.onMenuClosed} className={styles.Close} />
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={4} className={styles.logoContainer}>
         <Link to="/">
           <img className={styles.LogoImage} src={logo} alt="Logo" />
         </Link>
       </Grid>
+      <Hidden mdUp>
+        <Grid item xs={3} />
+        <Grid item xs={2} className={styles.searchContainer}>
+          <SearchComponent />
+        </Grid>
+      </Hidden>
     </Grid>
     <Grid container className={styles.menuOptions}>
       <Grid item xs={2} />
@@ -38,7 +46,7 @@ const SidebarMenu = props => (
       </Grid>
     </Grid>
     <Grid container className={styles.shopContainer}>
-      <Grid item xs={1} />
+      <Grid item xs={false} md={1} />
       <Grid item xs={3} className={styles.shop}>
         <a
           href="https://ufcstore.com/?utm_campaign=NA&utm_medium=header&utm_source=ufccom"
@@ -48,6 +56,12 @@ const SidebarMenu = props => (
           SHOP+
         </a>
       </Grid>
+      <Hidden mdUp>
+        <Grid item xs={6} />
+        <Grid item xs={2}>
+          <ShareIcon />
+        </Grid>
+      </Hidden>
     </Grid>
   </div>
 );
