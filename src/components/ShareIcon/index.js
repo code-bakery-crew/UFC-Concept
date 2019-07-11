7add83a0dfb91508ada9ddf62534c1b50e548b42
Jaco <jace3k@gmail.com>
2019-07-12 00:02:37 +0200

    added notifications

--- src/components/ShareIcon/index.js
+++ src/components/ShareIcon/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Share from "@material-ui/icons/Share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import Snackbar from '@material-ui/core/Snackbar';
 
 import styles from "./styles.module.css";
 
 const ShareIcon = () => {
-  //Alert to be changed to notification
+  const [notifyOpen, setNotifyOpen] = React.useState(false);
 
   return (
     <div className={styles.shareContainer}>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        open={notifyOpen}
+        autoHideDuration={2000}
+        onClose={() => setNotifyOpen(false)}
+        message="URL copied to clipboard!"
+      />
       <CopyToClipboard
         text={window.location.href}
-        onCopy={() => alert("Copied!")}
+        onCopy={() => setNotifyOpen(true)}
       >
         <Share className={styles.shareIcon} />
       </CopyToClipboard>
